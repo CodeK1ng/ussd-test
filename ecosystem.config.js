8bb0ebb73088e7b6fcfd540f4df9fc9bfe0ea1f5
malama <charlesmalama89@gmail.com>
2022-03-22 17:34:48 +0200

    Added configs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
       host : '41.175.8.68',
       ref  : 'origin/main',
       repo : 'https://github.com/CodeK1ng/ussd-test.git',
-      path : '/var/www/html/ussd-test',
+      path : '/var/www/html',
       "post-deploy" : "npm install"
     }
   }
